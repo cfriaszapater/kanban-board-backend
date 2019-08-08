8067c116f2e75dfd4faa8beb80905312893e0c23
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-08 12:40:26 +0200

    add morgan logger middleware

--- helloexpress/index.js
+++ helloexpress/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var morgan = require('morgan');
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
