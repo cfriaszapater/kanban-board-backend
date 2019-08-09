41099998be490f9a181c1cb38399c1d75c6663fd
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-09 09:20:09 +0200

    add routes and skeleton controllers

--- express-locallibrary/app.js
+++ express-locallibrary/app.js
@@ -6,26 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Set up mongoose connection
-dbConnect();
+viewEngineSetup();
+dbConnectionSetup();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -45,7 +35,19 @@ app.use(function (err, req, res, next) {
 
 module.exports = app;
 
-function dbConnect () {
+function viewEngineSetup () {
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({
+    extended: false
+  }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+}
+
+function dbConnectionSetup () {
   var mongoose = require('mongoose');
   var mongoDB = 'mongodb+srv://admin:admin@cluster0-pmxkl.azure.mongodb.net/local_library?retryWrites=true&w=majority';
   mongoose.connect(mongoDB, {
