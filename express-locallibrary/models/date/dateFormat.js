199827fedbe70edb5f3187e0fca2de28b1dbbccf
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-12 16:59:45 +0200

    fix: show empty when there is no date

--- express-locallibrary/models/date/dateFormat.js
+++ express-locallibrary/models/date/dateFormat.js
@@ -1,6 +1,9 @@
 var moment = require('moment');
 
 function dateFormat (date) {
+  if (date == null) {
+    return '';
+  }
   return moment(date).format('DD/MM/YYYY');
 }
 module.exports = dateFormat;
