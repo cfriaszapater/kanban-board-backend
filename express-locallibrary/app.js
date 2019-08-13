61fa81863bf40e35ce21814ae765704b1d2e9cce
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-13 15:40:05 +0200

    use debug module

--- express-locallibrary/app.js
+++ express-locallibrary/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var debug = require('debug')('express-locallibrary:app');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -53,7 +54,7 @@ function dbConnectionSetup () {
   mongoose.connect(mongoDB, {
     useNewUrlParser: true
   })
-    .then(console.log('connected to db'));
+    .then(debug('connected to db'));
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 }
