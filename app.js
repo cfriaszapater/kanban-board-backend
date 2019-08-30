db93dc4517aadfb39d37298aa2c9e9a3432f2a85
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-30 09:02:19 +0200

    update dev cluster DB password

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 /* jshint esversion: 8 */
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var morgan = require('morgan');
-var mongoose = require('mongoose');
-var debug = require('debug')('express-locallibrary:app');
-var bunyan = require('bunyan');
-var log = bunyan.createLogger({ name: 'express-locallibrary' });
-var compression = require('compression');
-var helmet = require('helmet');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var morgan = require("morgan");
+var mongoose = require("mongoose");
+var debug = require("debug")("express-locallibrary:app");
+var bunyan = require("bunyan");
+var log = bunyan.createLogger({ name: "express-locallibrary" });
+var compression = require("compression");
+var helmet = require("helmet");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var catalogRouter = require('./routes/catalog');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -27,50 +27,54 @@ app.use(compression());
 viewEngineSetup();
 dbConnectionSetup();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
 
-function viewEngineSetup () {
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'pug');
-  app.use(morgan('dev'));
+function viewEngineSetup() {
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "pug");
+  app.use(morgan("dev"));
   app.use(express.json());
-  app.use(express.urlencoded({
-    extended: false
-  }));
+  app.use(
+    express.urlencoded({
+      extended: false
+    })
+  );
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, "public")));
 }
 
-function dbConnectionSetup () {
-  mongoose.connect(dbUri(), {
-    useNewUrlParser: true
-  })
-    .then(debug('connected to db'));
+function dbConnectionSetup() {
+  mongoose
+    .connect(dbUri(), {
+      useNewUrlParser: true
+    })
+    .then(debug("connected to db"));
   var db = mongoose.connection;
-  db.on('error', (err) => log.error('DB connection error: %s', err));
+  db.on("error", err => log.error("DB connection error: %s", err));
 }
 
-function dbUri () {
-  var devUri = 'mongodb+srv://admin:admin@cluster0-pmxkl.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+function dbUri() {
+  var devUri =
+    "mongodb+srv://admin:almendrusc0@cluster0-pmxkl.azure.mongodb.net/local_library?retryWrites=true&w=majority";
   return process.env.MONGODB_URI || devUri;
 }
