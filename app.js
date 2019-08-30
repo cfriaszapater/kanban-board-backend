bebb50bba18209c579d9cb3f450e3f26e5d66aab
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-30 09:05:40 +0200

    update dev cluster DB password

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ function dbConnectionSetup() {
 
 function dbUri() {
   var devUri =
-    "mongodb+srv://admin:almendrusc0@cluster0-pmxkl.azure.mongodb.net/local_library?retryWrites=true&w=majority";
+    "mongodb+srv://admin:DZWkrlyCyhLimrez@cluster0-pmxkl.azure.mongodb.net/local_library?retryWrites=true&w=majority";
   return process.env.MONGODB_URI || devUri;
 }
